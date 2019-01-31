852933fa1e4262abbe4c70de903e56bf9d4ac7dc
Ashwani Kumar <akumar@monmouthcollege.edu>
2019-01-30 18:32:03 -0600

    Basic MapActivity working

--- app/src/main/java/com/example/admin/campusonphone/MapActivity.java
+++ app/src/main/java/com/example/admin/campusonphone/MapActivity.java
@@ -4,13 +4,31 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MapActivity extends AppCompatActivity {
-    private static final String TAG = "MapActivity";
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        // get the intent that activates this activity
-        Intent intent = getIntent();
+        Intent intent= getIntent();
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(MapActivity.this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng home = new LatLng(40.914355, -90.638366);
+        mMap.addMarker(new MarkerOptions().position(home).title("Monmouth College campus"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(home, 16));
     }
 }

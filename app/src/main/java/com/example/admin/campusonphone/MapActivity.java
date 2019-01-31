8cf13f51dced378fea027d8a2ffa240b029e4f8c
Ashwani Kumar <akumar@monmouthcollege.edu>
2019-01-30 19:48:11 -0600

    Enabled location tracking

--- app/src/main/java/com/example/admin/campusonphone/MapActivity.java
+++ app/src/main/java/com/example/admin/campusonphone/MapActivity.java
@@ -1,6 +1,11 @@
 package com.example.admin.campusonphone;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,7 +17,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
-private GoogleMap mMap;
+    private static final String TAG = MapActivity.class.getSimpleName();
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,12 +30,53 @@ private GoogleMap mMap;
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(MapActivity.this);
     }
-
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng home = new LatLng(40.914355, -90.638366);
-        mMap.addMarker(new MarkerOptions().position(home).title("Monmouth College campus"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(home, 16));
+        LatLng mcCampus = new LatLng(40.913600, -90.638606);
+        mMap.addMarker(new MarkerOptions().position(mcCampus).title("Monmouth College campus"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mcCampus, 17));
+        //mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        // we can add menu here to select MapType
+
+        enableMyLocation();//check permission for location
     }
+
+    /**
+     * Checks for location permissions, and requests them if they are missing.
+     * Otherwise, enables the location layer.
+     */
+    private void    enableMyLocation(){
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+        == PackageManager.PERMISSION_GRANTED){
+            mMap.setMyLocationEnabled(true);
+        }else {
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        // Check if location permissions are granted and if so enable the
+        // location data layer.
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION:
+                if (grantResults.length > 0
+                        && grantResults[0]
+                        == PackageManager.PERMISSION_GRANTED) {
+                    enableMyLocation();
+                    break;
+                }
+        }
+    }
+
 }
